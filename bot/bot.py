4855f74cde9942e71b4eb24439136e99f44210bd
artemii <kavrese@gmail.com>
2023-11-08 22:03:48 +0300

    refactored bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -2,18 +2,19 @@ import asyncio
 from . import bot, dp, logger, handlers
 
 
-def on_startup(_):
-    print(f'Бот запущен!')
+def on_startup():
+    logger.success('Bot Started')
 
 
-async def on_shutdown(_):
-    pass
+def on_shutdown():
+    logger.success('Bot Finished')
 
 
 async def main():
+    on_startup()
     await dp.start_polling(bot)
 
 
 def run():
-    logger.success('Bot Started')
-    asyncio.run(main())
+    asyncio.get_event_loop().run_until_complete(main())
+    on_shutdown()
